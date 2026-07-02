be52b42a96311494c2a95907494b6ddacd3e2a48
billytkid <morrish@gmail.com>
2026-07-02 08:08:18 +0100

    Add files via upload

--- iperf3_server/rootfs/run.sh
+++ iperf3_server/rootfs/run.sh
@@ -3,29 +3,37 @@ set -e
 
 PORT=$(bashio::config 'port')
 
-MQTT_HOST=$(bashio::services mqtt "host")
-MQTT_PORT=$(bashio::services mqtt "port")
-MQTT_USER=$(bashio::services mqtt "username")
-MQTT_PASS=$(bashio::services mqtt "password")
+MQTT_OK=false
+if bashio::services.available "mqtt"; then
+    MQTT_HOST=$(bashio::services mqtt "host")
+    MQTT_PORT=$(bashio::services mqtt "port")
+    MQTT_USER=$(bashio::services mqtt "username")
+    MQTT_PASS=$(bashio::services mqtt "password")
+    MQ_ARGS=(-h "${MQTT_HOST}" -p "${MQTT_PORT}" -u "${MQTT_USER}" -P "${MQTT_PASS}")
+    MQTT_OK=true
+else
+    bashio::log.warning "No MQTT service available - entities will not be published, iperf3 will still run"
+fi
 
 DEVICE_ID="iperf3_server"
 DISC_PREFIX="homeassistant"
 
-MQ_ARGS=(-h "${MQTT_HOST}" -p "${MQTT_PORT}" -u "${MQTT_USER}" -P "${MQTT_PASS}")
-
 publish() {
-    mosquitto_pub "${MQ_ARGS[@]}" -r -t "$1" -m "$2"
+    if [ "${MQTT_OK}" = true ]; then
+        mosquitto_pub "${MQ_ARGS[@]}" -r -t "$1" -m "$2" || bashio::log.warning "MQTT publish to $1 failed"
+    fi
 }
 
-bashio::log.info "Publishing MQTT discovery for iperf3 sensors"
+if [ "${MQTT_OK}" = true ]; then
+    bashio::log.info "Publishing MQTT discovery for iperf3 sensors"
+    DEVICE_JSON='{"identifiers":["iperf3_server"],"name":"iperf3 Server","model":"iperf3","manufacturer":"ESnet"}'
 
-DEVICE_JSON='{"identifiers":["iperf3_server"],"name":"iperf3 Server","model":"iperf3","manufacturer":"ESnet"}'
+    publish "${DISC_PREFIX}/sensor/${DEVICE_ID}/sending_host/config" \
+    "{\"name\":\"iperf3 Sending Host\",\"unique_id\":\"iperf3_sending_host\",\"state_topic\":\"iperf3/sending_host\",\"icon\":\"mdi:ip-network\",\"device\":${DEVICE_JSON}}"
 
-publish "${DISC_PREFIX}/sensor/${DEVICE_ID}/sending_host/config" \
-"{\"name\":\"iperf3 Sending Host\",\"unique_id\":\"iperf3_sending_host\",\"state_topic\":\"iperf3/sending_host\",\"icon\":\"mdi:ip-network\",\"device\":${DEVICE_JSON}}"
-
-publish "${DISC_PREFIX}/sensor/${DEVICE_ID}/throughput_mbps/config" \
-"{\"name\":\"iperf3 Throughput\",\"unique_id\":\"iperf3_throughput_mbps\",\"state_topic\":\"iperf3/throughput_mbps\",\"unit_of_measurement\":\"Mbit/s\",\"device_class\":\"data_rate\",\"state_class\":\"measurement\",\"icon\":\"mdi:speedometer\",\"device\":${DEVICE_JSON}}"
+    publish "${DISC_PREFIX}/sensor/${DEVICE_ID}/throughput_mbps/config" \
+    "{\"name\":\"iperf3 Throughput\",\"unique_id\":\"iperf3_throughput_mbps\",\"state_topic\":\"iperf3/throughput_mbps\",\"unit_of_measurement\":\"Mbit/s\",\"device_class\":\"data_rate\",\"state_class\":\"measurement\",\"icon\":\"mdi:speedometer\",\"device\":${DEVICE_JSON}}"
+fi
 
 bashio::log.info "Starting iperf3 server loop on port ${PORT}"
 
